3f9f8c43315dab0d98852aea1e15aa4ee57c8100
sumayya007 <sumayya.kareem6@gmail.com>
2019-03-25 08:52:54 +0400

    importing project

---
+++ jav.js
@@ -0,0 +1,12 @@
+// show cart
+(function(){
+/*const cartInfo=document.getElementById("cart-info");
+const cart=document.getElementById("cart");
+ 
+cartInfo.addEventListener("click",function(){
+cart.classList.toggle("show-cart";)
+})*/
+//alert("unniii");
+
+})();
+//hold items added to the cart
